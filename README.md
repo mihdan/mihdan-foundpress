f508a04b0cd4970cb115556ea8f4534ab12fc3a4
mihdan <mihdan@yandex.ru>
2017-04-19 10:17:55 +0300

    Инициализация проекта на GitHub

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# mihdan-foundpress
-Стартовая тема на основе ZURB Foundation 6 и WordPress
+# Mihdan: FoundPress
+Стартовая тема на основе ZURB Foundation 6 и Underscores WordPress
+
+# Благодарность
+- Каркас темы сгенерирован при помощи сервиса [underscores.me](http://underscores.me/)
+- Визуальная составляющая собрана на основе CSS фреймворка [ZURB Foundation](http://foundation.zurb.com)
---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Mihdan: FoundPress Theme Customizer
+ *
+ * @package Mihdan:_FoundPress
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function mihdan_foundpress_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'mihdan_foundpress_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function mihdan_foundpress_customize_preview_js() {
+	wp_enqueue_script( 'mihdan_foundpress_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'mihdan_foundpress_customize_preview_js' );
